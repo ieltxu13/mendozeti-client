202e8fe7c0ab70f3d675b73cb0fe722e836625d2
ieltxu13 <ieltxu.alganaras@gmail.com>
2017-05-29 23:42:46 -0300

    mejora filtros

--- src/app/filtro-estado.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroEstadoPipe } from './filtro-estado.pipe';
-
-describe('FiltroEstadoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroEstadoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
